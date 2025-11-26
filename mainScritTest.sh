2843eb0269c6268bdce8e94c6a0f0dc90e27ab97
christianwildcodeschool-dotcom <christian.a.wildcodeschool@gmail.com>
2025-11-26 20:50:29 +0100

    Create mainScritTest.sh

---
+++ mainScritTest.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+
+# Scripts 
+
+userScript='source scriptUserP2.sh'
+groupScript='source scriptGroups.sh'
+
+
+# Fonctions
+while true; do
+
+    #=====================================================
+    # VARIABLES
+    #=====================================================
+    userChoiceMainMenu=""
+    userMainMenu=""
+
+
+
+    #=====================================================
+    # FONCTIONS
+    #=====================================================
+    function mainMenu(){
+
+
+        echo ""
+        echo "##################################################"
+        echo "#                MENU PRINCIPAL                  #"
+        echo "##################################################"
+        echo "#                                                #"
+        echo "#  Choisissez une action :                       #"
+        echo "#                                                #"
+        echo "#  1. Gestion des Utilisateurs                   #"
+        echo "#  2. Gestions des Ordinateurs                   #"
+        echo "#  3. Informations Système                       #"
+        echo "#  4. Recherche dans les logs                    #"
+        echo "#  5. Quitter                                    #"
+        echo "#                                                #"
+        echo "##################################################"
+        echo ""
+
+        read -p "# Choisissez une action : " userChoiceMainMenu
+
+        case $userChoiceMainMenu in 
+
+            1) 
+                userMainMenu
+                ;;
+
+            2)
+                echo "test ordi"
+                ;;
+
+            3)
+                echo "test information"
+                ;;
+            4) 
+                echo "test logs"
+                ;;
+            5)
+                echo "Fermeture du programme"
+                exit 0
+        esac
+
+    }
+
+function userMainMenu(){
+
+    echo ""
+    echo "##################################################"
+    echo "#                MENU UTILISATEUR                #"
+    echo "##################################################"
+    echo "#                                                #"
+    echo "#  Choisissez une action :                       #"
+    echo "#                                                #"
+    echo "#  1. Utilisateurs                               #"
+    echo "#  2. Groupes                                    #"
+    echo "#  3. Journalisation                             #"
+    echo "#  4. Menu Principal                             #"
+    echo "#                                                #"
+    echo "##################################################"
+    echo ""
+
+    read -p "# Choisissez une action : " userMainMenu
+
+    case $userMainMenu in
+
+        1) 
+            $userScript
+            ;;
+        
+        2)
+            $groupScript 
+            ;;
+
+        3) 
+            echo "test2"
+            ;;
+        
+        4)
+            mainMenu
+            ;;
+    esac
+
+
+
+}
+
+# function computerMainMenu(){
+
+# }
+
+# function informationMainMenu(){
+
+# }
+
+# function logsMainMenu(){
+
+# }
+mainMenu
+
+done

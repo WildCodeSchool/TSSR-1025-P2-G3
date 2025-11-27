a02329031b0220b19291dcd8ca0e78f18637719b
Safiullah ABDULLAH <safiullah_007@yahoo.com>
2025-11-27 11:12:48 +0100

    Doning some changes

--- Ressources/Scripts/script_gestion_dossier_bash.sh
+++ Ressources/Scripts/script_gestion_dossier_bash.sh
@@ -1,108 +1,113 @@
-#!/bin/bash
+while true;
+do
+
+    ##################################### Menu Gestion Répertoire Ubuntu ####################################
+    function repertoireMenu(){
+
+            echo "##################################################"
+            echo "#              Gestion Répertoires Ubuntu        #"
+            echo "##################################################"
+            echo "#                                                #"
+            echo "#  Choisissez une action :                       #"
+            echo "#                                                #"
+            echo "#  1. Crée répertoire                            #"
+            echo "#  2. Supprimer répertoire                       #"
+            echo "#  3. Retour Menu président (sortire)            #"
+            echo "#                                                #"
+            echo "##################################################"
+
+            read -p "# Choisissez une option : " choix
 
 
+            case "$choix" in
 
+                1) 
+                    fonction_creer_dossier 
+                    ;;
+                2) 
+                    fonction_supprimer_dossier 
+                    ;;
+                3) 
+                    echo " Retour Menu principal "
+                    mainMenu 
+                    ;;
 
+            esac
 
-
+        }
 
 
 ################################## Fonction demander chemin #######################################
 
-fonction_demander_chemin() {
-        echo "##################################################"
-        echo "#          Entre le chemin du dossier:           #"
-        echo "##################################################"
-        read chemindossier
+    function fonction_demander_chemin(){
+            echo "##################################################"
+            echo "#          Entre le chemin du dossier:           #"
+            echo "##################################################"
+            read -r chemindossier
 
-# Vérifie si l'utilisateur a pas laissé vide
+    # Vérifie si l'utilisateur a pas laissé vide
 
-    if [ -z "$chemindossier" ]
-        then 
-            echo " ********* Aucun chemin saisi ************ "
-            return 1
-    fi
-    return 0
-}
+        if [ -z "$chemindossier" ]
+            then 
+                echo " ********* Aucun chemin saisi ************ "
+                return 1
+        fi
+        return 0
+    }
 
-################################## Fonction création répertoire #####################################
+    ################################## Fonction création répertoire #####################################
 
-fonction_creer_dossier() {
-clear
+    function fonction_creer_dossier(){
+        clear
         echo "##################################################"
         echo "#              Création de dossier               #"
         echo "##################################################"
         fonction_demander_chemin
-# Vérifie si l'utilisateur a pas laissé vide
+        # Vérifie si l'utilisateur a pas laissé vide
 
-# Vérificaton si le répertoire existe déja
+        # Vérificaton si le répertoire existe déja
 
-    if [ -d "$chemindossier" ]
-        then 
-            echo " ******* Le Dossier existe déja pas de création ********* "
-            
-        else
-            echo " Création do Dossier"
-            mkdir "$chemindossier"
+        if [ -d "$chemindossier" ]
+            then 
 
-            # vrification que dossier à bien été créé
-            if [ $? -eq 0 ]
-                then echo " ******** Le dossier à bien été créé ********** "
-                else echo " ******** Le dossier à été pas créé  ********** "
-            fi
-    fi
-}
+                echo " ******* Le Dossier existe déja pas de création ********* "
 
-#################################### Fonction Supprimer un Dossier #####################################
+            else
+                echo " Création do Dossier"
+                mkdir "$chemindossier"
 
-fonction_supprimer_dossier() {
-clear
-        echo "##################################################"
-        echo "#              Suppressin de dossier             #"
-        echo "##################################################"
+                # vrification que dossier à bien été créé
+                if [ $? -eq 0 ]
+                    then echo " ******** Le dossier à bien été créé ********** "
+                    else echo " ******** Le dossier à été pas créé  ********** "
+                fi
+        fi
+    }
 
-        fonction_demander_chemin
+    #################################### Fonction Supprimer un Dossier #####################################
 
-# Vérificaton si le dossier exist déja
+    function fonction_supprimer_dossier(){
+    clear
+            echo "##################################################"
+            echo "#              Suppressin de dossier             #"
+            echo "##################################################"
 
-    if [ ! -d "$chemindossier" ]
-        then 
-            echo " ****** Le Dossier n'existe, pas de Supprition ******* "
-        else
-            rm -r "$chemindossier" 2>/dev/null
-            if [ $? -eq 0 ]
-                then echo " ********* Le dossier à bien été Suprimé ********* "
-                else echo " ********* Le dossier à été pas Suprimé ********** "
-            fi
-    fi
+            fonction_demander_chemin
 
-}
+    # Vérificaton si le dossier exist déja
 
-##################################### Menu Gestion Répertoire Ubuntu ####################################
+        if [ ! -d "$chemindossier" ]
+            then 
+                echo " ****** Le Dossier n'existe, pas de Supprition ******* "
+            else
+                rm -r "$chemindossier" 2>/dev/null
+                if [ $? -eq 0 ]
+                    then echo " ********* Le dossier à bien été Suprimé ********* "
+                    else echo " ********* Le dossier à été pas Suprimé ********** "
+                fi
+        fi
 
-while true;
-    do
-    
-        echo "##################################################"
-        echo "#              Gestion Répertoires Ubuntu        #"
-        echo "##################################################"
-        echo "#                                                #"
-        echo "#  Choisissez une action :                       #"
-        echo "#                                                #"
-        echo "#  1. Crée répertoire                            #"
-        echo "#  2. Supprimer répertoire                       #"
-        echo "#  3. Retour Menu président (sortire)            #"
-        echo "#                                                #"
-        echo "##################################################"
-
-        read choix
-
-        case "$choix" in
-        
-            1) fonction_creer_dossier ;;
-            2) fonction_supprimer_dossier ;;
-            3) echo " Retour Menu président "; exit ;;
-        esac
+    }
+   
+    repertoireMenu
 done 
-
-

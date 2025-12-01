12a8b0368660f3c5bb08a051ae20845d0dbfa407
christianwildcodeschool-dotcom <christian.a.wildcodeschool@gmail.com>
2025-12-01 12:02:16 +0100

    Update mainScript.sh

--- mainScript.sh
+++ mainScript.sh
@@ -1,122 +1,387 @@
 #!/bin/bash
 
+# Script Bash principal du Projet 2
 
-# Scripts 
+#=====================================================
+# CHARGEMENT DES MODULES
+#=====================================================
+source scriptUsers.sh
+source scriptGroups.sh
+source scriptFolder.sh
 
-userScript='source scriptUserP2.sh'
-groupScript='source scriptGroups.sh'
+#=====================================================
+# VARIABLES DE CONNEXION
+#=====================================================
 
+export connexionMode=""
+export remoteUser=""
+export remoteComputer=""
+export portSSH=""
 
-# Fonctions
-while true; do
+#=====================================================
+# MENU EXECUTION LOCAL OU SSH
+#=====================================================
 
-    #=====================================================
-    # VARIABLES
-    #=====================================================
-    userChoiceMainMenu=""
-    userMainMenu=""
-
-
-
-    #=====================================================
-    # FONCTIONS
-    #=====================================================
-    function mainMenu(){
-
-
-        echo ""
-        echo "##################################################"
-        echo "#                MENU PRINCIPAL                  #"
-        echo "##################################################"
-        echo "#                                                #"
-        echo "#  Choisissez une action :                       #"
-        echo "#                                                #"
-        echo "#  1. Gestion des Utilisateurs                   #"
-        echo "#  2. Gestions des Ordinateurs                   #"
-        echo "#  3. Informations Système                       #"
-        echo "#  4. Recherche dans les logs                    #"
-        echo "#  5. Quitter                                    #"
-        echo "#                                                #"
-        echo "##################################################"
-        echo ""
-
-        read -p "# Choisissez une action : " userChoiceMainMenu
-
-        case $userChoiceMainMenu in 
-
-            1) 
-                userMainMenu
-                ;;
-
-            2)
-                echo "test ordi"
-                ;;
-
-            3)
-                echo "test information"
-                ;;
-            4) 
-                echo "test logs"
-                ;;
-            5)
-                echo "Fermeture du programme"
-                exit 0
-        esac
-
-    }
-
-function userMainMenu(){
+function chooseExecutionMode() {
 
     echo ""
-    echo "##################################################"
-    echo "#                MENU UTILISATEUR                #"
-    echo "##################################################"
-    echo "#                                                #"
-    echo "#  Choisissez une action :                       #"
-    echo "#                                                #"
-    echo "#  1. Utilisateurs                               #"
-    echo "#  2. Groupes                                    #"
-    echo "#  3. Menu Principal                             #"
-    echo "#                                                #"
-    echo "##################################################"
+    echo "╭────────────────────────────────────────────────╮"
+    echo "│           MENU EXECUTION LOCAL OU SSH          │"
+    echo "├────────────────────────────────────────────────┤"
+    echo "│                                                │"
+    echo "│  1. Exécution locale                           │"
+    echo "│  2. Exécution distante (SSH)                   │"
+    echo "│  3. Quitter                                    │"
+    echo "│                                                │"
+    echo "╰────────────────────────────────────────────────╯"
     echo ""
 
-    read -p "# Choisissez une action : " userMainMenu
+    read -p "► Voulez-vous exécuter le script en Local ou à distance ? " executionMode
 
-    case $userMainMenu in
+    case $executionMode in
 
-        1) 
-            $userScript
-            ;;
-        
-        2)
-            $groupScript 
-            ;;
+    1)
+        connexionMode="local"
+        export connexionMode
 
-        3) 
-            echo "test2"
-            ;;
-        
-        4)
-            mainMenu
-            ;;
+        echo "► Exécution du script sur la machine hôte"
+        echo ""
+        ;;
+
+    2)
+
+        connexionMode="ssh"
+        export connexionMode
+
+        echo ""
+        read -p "► Entrez une Adresse IP ou Hostname : " remoteComputer
+        read -p "► Entrez un Nom d'utilisateur : " remoteUser
+        read -p "► Entrez un Port : " portSSH
+        echo ""
+
+        export remoteComputer
+        export remoteUser
+        export portSSH
+
+        echo "► Connexion en cours : $remoteUser@$remoteComputer"
+        echo ""
+
+        if ssh -o BatchMode=yes -o ConnectTimeout=5 -p"$portSSH" "$remoteUser@$remoteComputer" "echo 'Test connexion OK'" >/dev/null 2>&1; then
+
+            echo "► Connexion SSH réussie à $remoteUser@$remoteComputer"
+            echo ""
+        else
+
+            echo "► Impossible de se connecter à $remoteUser@$remoteComputer"
+            echo ""
+            chooseExecutionMode
+        fi
+        ;;
+
+    3)
+
+        echo "► Fermeture du script"
+        exit 0
+        ;;
+
+    *)
+        echo "► Entrée Invalide !\n Retour au menu"
+        echo ""
+        chooseExecutionMode
+        ;;
     esac
-
-
 
 }
 
-# function computerMainMenu(){
+#=====================================================
+# FONCTION DES COMMANDES
+#=====================================================
 
-# }
+# Fonction pour les commandes sans SUDO
+function command() {
 
-# function informationMainMenu(){
+    local cmd="$1"
 
-# }
+    if [ "$connexionMode" = "local" ]; then
+        bash -c "$cmd"
+    else
+        ssh -p "$portSSH" "$remoteUser@$remoteComputer" "$cmd"
+    fi
+}
 
-# function logsMainMenu(){
+# Fonction pour les commandes exigeant SUDO
+function sudo_command() {
 
-# }
+    local cmd="$1"
+
+    if [ "$connexionMode" = "local" ]; then
+        bash -c "sudo $cmd"
+    else
+        ssh -t -o logLevel=ERROR -p "$portSSH" "$remoteUser@$remoteComputer" "sudo $cmd"
+    fi
+}
+
+#=====================================================
+# MENU PRINCPAL
+#=====================================================
+
+function mainMenu() {
+
+    while true; do
+        echo ""
+        echo "╭──────────────────────────────────────────────────╮"
+        echo "│                  MENU PRICINPAL                  │"
+        echo "├──────────────────────────────────────────────────┤"
+        echo "│                                                  │"
+        echo "│  1. Gestion des Utilisateurs                     │"
+        echo "│  2. Gestions des Ordinateurs                     │"
+        echo "│  3. Informations Système                         │"
+        echo "│  4. Informations Utilisateur                     │"
+        echo "│  5. Journalisation                               │"
+        echo "│  6. Changer mode exécution                       │"
+        echo "│  7. Quitter                                      │"
+        echo "│                                                  │"
+        echo "╰──────────────────────────────────────────────────╯"
+        echo ""
+
+        read -p "► Choisissez une option : " userChoiceMainMenu
+
+        case $userChoiceMainMenu in
+
+        1)
+            userMainMenu
+            ;;
+
+        2)
+            computerMainMenu
+            ;;
+
+        3)
+            informationMainMenu
+            ;;
+
+        4)
+            informationUserMainMenu
+            ;;
+
+        5)
+            logsMainMenu
+            ;;
+
+        6)
+            chooseExecutionMode
+            ;;
+
+        7)
+            read -p "► Voulez-vous fermer le script ? (o/n) " fermetureScript
+            if [ "$fermetureScript" = "o" ]; then
+                exit 0
+            fi
+            ;;
+        esac
+    done
+
+}
+
+function userMainMenu() {
+
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│             MENU GESTION UTILISATEUR             │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Utilisateurs                                 │"
+    echo "│  2. Groupes                                      │"
+    echo "│  3. Menu Principal                               │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " userMainMenu
+
+    case $userMainMenu in
+
+    1)
+        userMenu
+        ;;
+
+    2)
+        fonc_menu_group
+        ;;
+
+    3)
+        mainMenu
+        ;;
+
+    *)
+        echo "► Entrée Invalide"
+        userMainMenu
+        ;;
+    esac
+
+}
+
+function computerMainMenu() {
+
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│             MENU GESTION ORDINATEUR              │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Répertoire                                   │"
+    echo "│  2. Redémarrer                                   │"
+    echo "│  3. Activer le Pare-Feu                          │"
+    echo "│  4. Prise de main à distance (CLI)               │"
+    echo "│  5. Exécuter un script                           │"
+    echo "│  6. Menu Principal                               │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " computerMainMenu
+
+    case $computerMainMenu in
+
+    1)
+        repertoireMenu
+        ;;
+
+    6)
+        mainMenu
+        ;;
+
+    *)
+        echo "► Entrée Invalide"
+        computerMainMenu
+        ;;
+    esac
+}
+
+function informationMainMenu() {
+
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│            MENU INFORMATIONS SYSTEME             │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Liste des utilisateurs                       │"
+    echo "│  2. Afficher adresse IP, masque, passerelle      │"
+    echo "│  3. Informations disque dur                      │"
+    echo "│  4. Version de l'OS                              │"
+    echo "│  5. Mises à jour critiques manquantes            │"
+    echo "│  6. Afficher marque/modèle de l'ordinateur       │"
+    echo "│  7. Statut UAC                                   │"
+    echo "│  8. Menu Principal                               │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " informationMainMenu
+
+    case $informationMainMenu in
+
+    1)
+
+        echo "blabla"
+        ;;
+
+    8)
+
+        mainMenu
+        ;;
+
+    *)
+        echo "► Entrée Invalide"
+        informationMainMenu
+        ;;
+
+    esac
+}
+
+function informationUserMainMenu() {
+
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│          MENU INFORMATIONS UTILISATEURS          │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Date dernière connexion                      │"
+    echo "│  2. Date dernière modification de mot de passe   │"
+    echo "│  3. Liste des sessions ouvertes                  │"
+    echo "│  4. Afficher les 5 derniers logins               │"
+    echo "│  5. Menu Principal                               │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " informationUserMenu
+
+    case $informationUserMenu in
+
+    1)
+
+        echo "blabla"
+        ;;
+    2)
+
+        echo "blabla"
+        ;;
+
+    5)
+        mainMenu
+        ;;
+
+    *)
+        echo "► Entrée Invalide"
+        informationUserMainMenu
+        ;;
+
+    esac
+}
+
+function logsMainMenu() {
+
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│               MENU JOURNALISATION                │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Recherche log Utilisateur                    │"
+    echo "│  2. Recherche log Ordinateur                     │"
+    echo "│  3. Menu Principal                               │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " logsMenu
+
+    case $logsMenu in
+
+    1)
+
+        echo "blabla"
+        ;;
+    2)
+
+        echo "blabla"
+        ;;
+    3)
+
+        mainMenu
+        ;;
+
+    *)
+        echo "► Entrée Invalide"
+        logsMainMenu
+        ;;
+
+    esac
+
+}
+
+#=====================================================
+# EXECUTION DU SCRIPT
+#=====================================================
+
+chooseExecutionMode
 mainMenu
-
-done

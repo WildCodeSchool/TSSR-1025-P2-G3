01c6a7600d6057416d2a662cef58d33650c0b41f
christianwildcodeschool-dotcom <christian.a.wildcodeschool@gmail.com>
2025-12-01 12:31:27 +0100

    Update scriptUser.sh

--- scriptUser.sh
+++ scriptUser.sh
@@ -3,286 +3,277 @@
 # Script de création, suppression d'utilisateur et modification de mot de passe utilisateurs.
 # Auteur : Christian
 
+#=====================================================
+# FONCTIONS
+#=====================================================
 
+# Sous menu "Utilisateurs"
+function userMenu() {
 
-while true;do
-
-    #=====================================================
-    # VARIABLES
-    #=====================================================
-    menuUser=""
-    menuAddUser=""
-    addUserCommand=""
-    userCreationChoice=""
-    delUserMenu=""
-    delUserChoice=""
-
-
-
-    #=====================================================
-    # FONCTIONS
-    #=====================================================
-
-    # Sous menu "Utilisateurs"
-    function userMenu(){
+    while true; do
 
         echo ""
-        echo "##################################################"
-        echo "#               MENU UTILISATEUR                 #"
-        echo "##################################################"
-        echo "#                                                #"
-        echo "#  Choisissez une action :                       #"
-        echo "#                                                #"
-        echo "#  1. Ajouter un utilisateur                     #"
-        echo "#  2. Supprimer un utilisateur                   #"
-        echo "#  3. Changer le mot de passe d'un Utilisateur   #"
-        echo "#  4. Afficher les Utilisateurs                  #"
-        echo "#  5. Retour au menu précédent                   #"
-        echo "#                                                #"
-        echo "##################################################"
+        echo "╭────────────────────────────────────────────────╮"
+        echo "│                MENU UTILISATEUR                │"
+        echo "├────────────────────────────────────────────────┤"
+        echo "│                                                │"
+        echo "│  1. Ajouter un utilisateur                     │"
+        echo "│  2. Supprimer un utilisateur                   │"
+        echo "│  3. Changer le mot de passe d'un Utilisateur   │"
+        echo "│  4. Afficher les Utilisateurs                  │"
+        echo "│  5. Retour au menu précédent                   │"
+        echo "│                                                │"
+        echo "╰────────────────────────────────────────────────╯"
         echo ""
 
+        read -p "► Choisissez une option : " menuUser
 
-        read -p "# Entrez un nombre : " menuUser
+        case $menuUser in
 
-        case $menuUser in 
+        1)
+            addUserMenu
+            ;;
 
-            1) 
-                addUserMenu
-                ;;
+        2)
+            deleteUserMenu
+            ;;
 
-            2)
-                deleteUserMenu
-                ;;
+        3)
+            changePasswordUserMenu
+            ;;
 
-            3)
-                changePasswordUser
-                ;;
+        4)
 
-            4)
+            echo ""
+            echo "► Liste des utilisateurs : "
+            command "awk -F':' '\$3 >= 1000 {print \$1, \$3}' /etc/passwd | sort -k 2"
+            ;;
 
-                echo ""
-                echo "Liste des utilisateurs : "
-                awk -F':' '$3 >= 1000 {print $1, $3}' /etc/passwd | sort -k 2
-                ;;
+        5)
+            return
+            ;;
 
-            5)
-                userMainMenu
-                ;;
+        *)
+            echo
+            echo -e "► ${RED}Entrée invalide! ${NC}"
+            userMenu
+            ;;
 
-            *)
-                echo "Entrée invalide"
-                userMenu
-                ;;
         esac
-    }
 
+    done
+}
 
-    # Ajouter un utilisateur
-    function addUserMenu(){
+# Ajouter un utilisateur
+function addUserMenu() {
 
-        echo ""
-        echo "##################################################"
-        echo "#              AJOUTER UTILISATEUR               #"
-        echo "##################################################"
-        echo "#                                                #"
-        echo "#  Choisissez une action :                       #"
-        echo "#                                                #"
-        echo "#  1. Saisir un nom d'utilisateur                #"
-        echo "#  2. Retour au menu précédent                   #"
-        echo "#                                                #"
-        echo "##################################################"
-        echo ""
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│               AJOUTER UTILISATEUR                │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Saisir un nom d'utilisateur                  │"
+    echo "│  2. Retour au menu précédent                     │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
 
-        read -p "# Entrez un nombre : " menuAddUser
+    read -p "► Choisissez une option : " menuAddUser
 
-        case $menuAddUser in
+    case $menuAddUser in
 
-            1)
-                userCreationChoice="o"
+    1)
+        userCreationChoice="o"
 
-                while [ "$userCreationChoice" = "o" ]; do
-                
-                    read -p "# Entrez un nom d'utilisateur : " addUserCommand        
-                    grep "^$addUserCommand:" /etc/passwd > /dev/null
+        while [ "$userCreationChoice" = "o" ]; do
 
-                    if [ $? = 1 ]
-                    then 
+            read -p "► Entrez un nom d'utilisateur : " addUserCommand
+            command "grep '^$addUserCommand:' /etc/passwd >/dev/null"
 
-                        read -p "# Voulez-vous créer l'utilisateur $addUserCommand ? (o/n) : " confirmUser
+            if [ $? = 1 ]; then
 
-                        if [ "$confirmUser" = "o" ]
-                        then 
-                            useradd "$addUserCommand"
-                            grep "^$addUserCommand:" /etc/passwd > /dev/null    
-                            echo ""      
-                            echo "## L'utilisateur $addUserCommand a été créé. ##"
-                            echo ""
+                read -p "► Voulez-vous créer l'utilisateur $addUserCommand ? (o/n) : " confirmUser
 
-                            read -p "# Voulez-vous créer un autre utilisateur ? (o/n) : " userCreationChoice
-                            echo ""
+                if [ "$confirmUser" = "o" ]; then
+                    sudo_command "useradd ${addUserCommand}"
 
-                        else 
-                            echo "Retour au menu précédent"
-                            echo ""
-                            userCreationChoice="n"
-                        fi
-
-                    else
-                        echo "L'utilisateur $addUserCommand existe déjà."
-                        echo "Retour au menu précédent"
+                    command "grep '^$addUserCommand:' /etc/passwd >/dev/null"
+                    if [ $? = 0 ]; then
                         echo ""
-                        addUserMenu
-                    fi
-                done
-                userMenu
-                ;;
+                        echo -e "► ${GREEN} L'utilisateur $addUserCommand a été créé. ${NC}"
+                        echo ""
 
-
-            2)
-                userMenu
-                ;;
-
-            *)
-                echo "Entrée Invalide!!!"
-                echo "Retour au menu précédent."
-                echo ""
-                addUserMenu
-                ;;
-        esac        
-
-        addUser
-
-    }
-
-    # Supprimer un utilisateur
-    function deleteUserMenu(){
-
-        echo ""
-        echo "##################################################"
-        echo "#            SUPPRIMER UTILISATEUR               #"
-        echo "##################################################"
-        echo "#                                                #"
-        echo "#  Choisissez une action :                       #"
-        echo "#                                                #"
-        echo "#  1. Saisir un nom d'utilisateur à supprimer    #"
-        echo "#  2. Afficher la liste des utilisateurs         #"
-        echo "#  3. Retour au menu précédent                   #"
-        echo "#                                                #"
-        echo "##################################################"
-        echo ""
-
-        read -p "Choisissez une option : " delUserMenu
-
-        case $delUserMenu in 
-
-            1)
-                delAnotherUser="o"
-
-                while [ "$delAnotherUser" = "o" ];do
-                    read -p "Entrez un nom d'utilisateur : " delUserCommand
-                    grep "^$delUserCommand:" /etc/passwd > /dev/null
-
-                    if [ $? = 1 ]
-                    then    
-                        echo "L'utilisateur n'existe pas."
-                        deleteUserMenu
-                    else 
-                        read -p "Souhaitez-vous supprimer l'utilisateur $delUserCommand ? (o/n) " delUserChoice
-
-                        if [ "$delUserChoice" = "o" ]
-                        then 
-                            userdel $delUserCommand
-                            echo "L'utilisateur $delUserCommand a été supprimé."
-                            read -p "Voulez-vous supprimer un autre utilisateur ? (o/n) :" delAnotherUser
-
-                        else
-                            echo "Retour au menu."
-                        fi
-                    fi
-                done
-                ;;
-
-            2) 
-                echo ""
-                echo "Liste des utilisateurs : "
-                awk -F':' '$3 >= 1000 {print $1, $3}' /etc/passwd | sort
-                deleteUserMenu
-                ;;
-
-            3) 
-                userMenu
-                ;;
-
-            *)
-                echo "Entrée invalide !"
-                deleteUserMenu
-                ;;
-
-        esac
-        deleteUserMenu 
-    }
-
-    # Changer mot de passe utilisateur
-    function changePasswordUser(){
-       
-        echo ""
-        echo "##################################################"
-        echo "#       CHANGER MOT DE PASSE UTILISATEUR         #"
-        echo "##################################################"
-        echo "#                                                #"
-        echo "#  Choisissez une action :                       #"
-        echo "#                                                #"
-        echo "#  1. Saisir un nom d'utilisateur                #"
-        echo "#  2. Afficher la liste des utilisateurs         #"
-        echo "#  3. Retour au menu précédent                   #"
-        echo "#                                                #"
-        echo "##################################################"
-        echo ""
-             
-        read -p "Choisissez une option : " changePasswordMenu
-
-
-        case $changePasswordMenu in
-
-            1) 
-                read -p "Entrer un nom d'utilisateur : " changePasswordUser
-                grep "^$changePasswordUser:" /etc/passwd > /dev/null
-
-                if [ $? = 1 ]
-                then
-                    echo "Cet utilisateur n'existe pas"
-                else 
-                    read -p "Souhaitez-vous changer le mot de passe de l'utilisateur $changePasswordUser ? (o/n) " changepasswordUserChoice
-                    if [ "$changepasswordUserChoice" = "o" ]
-                    then 
-                        passwd $changePasswordUser
-                        echo "Le mot de passe de l'utilisateur $changePasswordUser a été modifié."
+                        read -p "► Voulez-vous créer un autre utilisateur ? (o/n) : " userCreationChoice
+                        echo ""
                     else
-                        echo "Retour au menu."
+                        echo ""
+                        echo -e "► ${RED}L'utilisateur $addUserCommand n'a pas été créé. ${NC}"
                     fi
+
+                else
+                    echo ""
+                    echo "► Retour au menu précédent"
+                    echo ""
+                    userCreationChoice="n"
                 fi
-                ;;
 
-            2) 
+            else
                 echo ""
-                echo "Liste des utilisateurs : "
-                awk -F':' '$3 >= 1000 {print $1, $3}' /etc/passwd | sort
-                changePasswordUser
-                ;;
+                echo -e "► ${RED}L'utilisateur $addUserCommand existe déjà.${NC}"
+                echo "Retour au menu précédent"
+                echo ""
+                userCreationChoice="n"
+            fi
+        done
+        ;;
 
-            3) 
-                userMenu
-                ;;
+    2)
+        return
+        ;;
 
-            *)
-                echo "Entrée invalide !"
-                userMenu
-                ;;
-        esac
+    *)
+        echo "► Entrée Invalide!!!"
+        echo "► Retour au menu précédent."
+        echo ""
+        ;;
+    esac
 
+}
 
-    }
-    userMenu
+# Supprimer un utilisateur
+function deleteUserMenu() {
 
-done
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│              SUPPRIMER UTILISATEUR               │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Saisir un nom d'utilisateur à supprimer      │"
+    echo "│  2. Afficher la liste des utilisateurs           │"
+    echo "│  3. Retour au menu précédent                     │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " delUserMenu
+
+    case $delUserMenu in
+
+    1)
+        delAnotherUser="o"
+
+        while [ "$delAnotherUser" = "o" ]; do
+            read -p "► Entrez un nom d'utilisateur : " delUserCommand
+            command "grep '^$delUserCommand:' /etc/passwd >/dev/null"
+
+            if [ $? = 1 ]; then
+                echo ""
+                echo "► L'utilisateur n'existe pas."
+                echo ""
+            else
+                read -p "# Souhaitez-vous supprimer l'utilisateur $delUserCommand ? (o/n) " delUserChoice
+
+                if [ "$delUserChoice" = "o" ]; then
+
+                    sudo_command "userdel ${delUserCommand}"
+                    command "grep '^$delUserCommand:' /etc/passwd >/dev/null"
+
+                    if [ $? = 1 ]; then
+                        echo "► L'utilisateur $delUserCommand a été supprimé."
+                        read -p "Voulez-vous supprimer un autre utilisateur ? (o/n) " delAnotherUser
+                        echo ""
+                    else
+                        echo ""
+                        echo "► L'utilisateur $delUserCommand n'a pas été supprimé."
+                        echo ""
+                    fi
+
+                else
+                    echo "Retour au menu."
+                fi
+            fi
+        done
+        ;;
+
+    2)
+        echo ""
+        echo "► Liste des utilisateurs : "
+        command "awk -F':' '\$3 >= 1000 {print \$1, \$3}' /etc/passwd | sort"
+        echo ""
+        deleteUserMenu
+        ;;
+
+    3)
+        return
+        ;;
+
+    *)
+        echo "► Entrée invalide !"
+        ;;
+
+    esac
+}
+
+# Changer mot de passe utilisateur
+function changePasswordUserMenu() {
+
+    echo ""
+    echo "╭──────────────────────────────────────────────────╮"
+    echo "│         CHANGER MOT DE PASSE UTILISATEUR         │"
+    echo "├──────────────────────────────────────────────────┤"
+    echo "│                                                  │"
+    echo "│  1. Saisir un nom d'utilisateur                  │"
+    echo "│  2. Afficher la liste des utilisateurs           │"
+    echo "│  3. Retour au menu précédent                     │"
+    echo "│                                                  │"
+    echo "╰──────────────────────────────────────────────────╯"
+    echo ""
+
+    read -p "► Choisissez une option : " changePasswordMenu
+
+    case $changePasswordMenu in
+
+    1)
+
+        read -p "► Entrer un nom d'utilisateur : " changePasswordUser
+        command "grep '^$changePasswordUser:' /etc/passwd >/dev/null"
+
+        if [ $? = 1 ]; then
+            echo "► Cet utilisateur n'existe pas"
+
+        else
+
+            read -p "► Souhaitez-vous changer le mot de passe de l'utilisateur $changePasswordUser ? (o/n) " changepasswordUserChoice
+
+            if [ "$changepasswordUserChoice" = "o" ]; then
+
+                sudo_command "passwd $changePasswordUser"
+                echo "► Le mot de passe de l'utilisateur $changePasswordUser a été modifié."
+                echo ""
+
+            else
+
+                echo "► Retour au menu."
+
+            fi
+        fi
+        ;;
+
+    2)
+        echo ""
+        echo "► Liste des utilisateurs : "
+        command "awk -F':' '\$3 >= 1000 {print \$1, \$3}' /etc/passwd | sort"
+        changePasswordUserMenu
+        ;;
+
+    3)
+        return
+        ;;
+
+    *)
+        echo "► Entrée invalide !"
+        ;;
+
+    esac
+
+}

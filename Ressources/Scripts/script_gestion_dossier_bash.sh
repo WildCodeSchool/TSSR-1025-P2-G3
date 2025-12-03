e62f163fa8128015bf1beedeae147644c67e58a6
Safiullah ABDULLAH <safiullah_007@yahoo.com>
2025-12-03 21:39:38 +0100

    Enhance directory management script functionality
    Refactor directory management script with improved menu options and logging.

--- Ressources/Scripts/script_gestion_dossier_bash.sh
+++ Ressources/Scripts/script_gestion_dossier_bash.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#################################### Menu Gestion Répertoire ####################################
 
-##################################### Menu Gestion Répertoire Ubuntu ####################################
-repertoireMenu() {
+# fonction de gestion de répertoires
+gestion_repertoire_menu() {
+logEvent " menu gestion répertoire "
+
+# boucle menue gestion de répertoires
 while true; do
 
 echo "╭──────────────────────────────────────────────────╮"
@@ -9,41 +12,60 @@ echo "│               Gestion Répertoires                │"
 echo "├──────────────────────────────────────────────────┤"
 echo "│                                                  │"
 echo "│  1. Créer un répertoire                          │"
-echo "│  2. Supprimer un répertoire                      │"
+echo "│  2. Supprimer un répertotour au menire           │"
 echo "│  3. Retour au menu précédent                     │"
 echo "│                                                  │"
 echo "╰──────────────────────────────────────────────────╯"
 
+    # Demande de faire un choix
     read -p "# Choisissez une option : " choix
+    logEvent "$"
 
+    # structure case prend la valeur $choix de cherche et excécute 
     case "$choix" in
-        1)
-            fonction_creer_dossier
-            ;;
-        2)
-            fonction_supprimer_dossier
-            ;;
-        3)
-            echo "Retour au menu principal..."
-            exit 0
-            ;;
-        *)
-            echo " Option invalide."
-            ;;
+    
 
+        # fonction de creation de dossier
+        logEvent "Sélection creation de dossier" 
+        1) fonction_creer_dossier
+        ;;
+
+        # fonction de supprition de dossier
+        logEvent "Sélection supprition de dossier"
+        2) fonction_supprimer_dossier
+        ;;
+
+        # fonction gestion ordinateur menue précédent
+        logEvent "Sélection Retour au menu précédent"
+        3) echo "Retour au menu précédent"
+        return
+        ;;
+
+
+        # si autre chosse c'est un valide
+        logEvent "Sélection Option invalide"
+        *) echo " Option invalide."
+        ;;
     esac
     
     done
 }
 
 ################################## Fonction demander chemin #######################################
-
 fonction_demander_chemin() {
-    echo "► Entrez le chemin du dossier :"
-    read -r chemindossier
 
+    logEvent " Entrez le chemin du dossier "
+    echo "► Entrez le chemin du dossier :"
+
+    # Demande donner un chemin dossier
+    read -r chemindossier
+    logEvent "chemindossier"
+
+    # vérifier si au moin un chemin a ete saisie
     if [ -z "$chemindossier" ]; then
+        logEvent " Aucun chemin saisi, retour "
         echo "► Aucun chemin saisi"
+
         return 1
     fi
     return 0
@@ -52,16 +74,23 @@ fonction_demander_chemin() {
 ################################## Fonction création répertoire #####################################
 
 fonction_creer_dossier() {
+    logEvent " Création de dossier " 
     echo "► Création de dossier"
     fonction_demander_chemin || return
-
+    
+    # vérifier si le dossier existe
     if [ -d "$chemindossier" ]; then
+        logEvent " Le dossier existe déjà "
         echo "► Le dossier existe déjà"
-    else
+    # si le dossier existe pas créé le dossier 
+    else 
         mkdir "$chemindossier" 2>/dev/null
+        # vérifier si le dossier a bien été créé 
         if [ $? -eq 0 ]; then
+            logEvent " Le dossier a été créé"
             echo "► Le dossier a été créé"
         else
+            logEvent " Erreur : dossier non créé "
             echo "► Erreur : dossier non créé"
         fi
     fi
@@ -70,21 +99,44 @@ fonction_creer_dossier() {
 #################################### Fonction supprimer dossier #####################################
 
 fonction_supprimer_dossier() {
+
+    logEvent " Suppression de dossier "
     echo "► Suppression de dossier"
     fonction_demander_chemin || return
 
+    # vérifier si le dossier existe pas si existe supprime 
     if [ ! -d "$chemindossier" ]; then
+        logEvent " Le dossier n'existe pas " 
         echo "► Le dossier n'existe pas"
     else
         rm -r "$chemindossier" 2>/dev/null
+        #vérifier si le dossier à bien été supprimé
         if [ $? -eq 0 ]; then
+            logEvent " Dossier supprimé "
             echo "► Dossier supprimé"
         else
+            logEvent " dossier non supprimé "
             echo "► Erreur : dossier non supprimé"
         fi
     fi
 }
 
-#################################### Boucle principale ##############################################
+###################################### Fonction redémarrage #######################################
 
-    repertoireMenu
+#fonction de redemarage poste distante
+fonction_redemarrage() {
+    # demande redémarrer la machine en ssh
+    logEvent " connexion en ssh "
+    sudo ssh -p "$portSSH" "$remoteUser@$remoteComputer"
+    
+    # vérifier si la commande avant (ssh) a bien été exécuté je fais redémarrer l'ordinateur, si non je donne la message erreur
+    if [ $? -eq 0 ]; then
+    logEvent "conextion fait avec succès l'ordinateur va redémarre"
+	echo "► l'ordinateur va redémarrer "
+    sudo reboot
+    else 
+    logEvent " ► Erreur ssh ou la commande redémarrage n'est pas fonctionné "
+	echo "► Erreur : commande n'est pas fonctionné "
+	fi
+    
+}

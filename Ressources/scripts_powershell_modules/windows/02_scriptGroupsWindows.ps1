bf744ad9b6225f94e7f6fecdaae1cd8448c5f6b4
christianwildcodeschool-dotcom <christian.a.wildcodeschool@gmail.com>
2025-12-12 21:42:37 +0100

    Add files via upload

---
+++ Ressources/scripts_powershell_modules/windows/02_scriptGroupsWindows.ps1
@@ -0,0 +1,194 @@
+﻿# Script Gestion de groupes Windows en Powershell
+
+
+# Liste des fonctions :
+# 1. Menu Groupe
+# 2. Ajouter un utilisateur au groupe Admin
+# 3. Ajouter un utilisateur à un groupe
+# 4. Supprimer un utilisateur d'un groupe
+
+
+#==============================================================
+# 1 - MENU GROUPE
+#==============================================================
+function gestion_menu_group_windows {
+
+    logEvent "MENU_GROUPE"
+    
+    while ($true) {
+
+        Write-Host ""
+        Write-Host "╭──────────────────────────────────────────────────╮"
+        Write-Host "│                   MENU GROUPE                    │"
+        Write-Host "├──────────────────────────────────────────────────┤"
+        Write-Host "│                                                  │"
+        Write-Host "│  1. Ajouter un utilisateur au groupe Admin       │"
+        Write-Host "│  2. Ajouter un utilisateur à un groupe           │"
+        Write-Host "│  3. Retirer un utilisateur d'un groupe           │"
+        Write-Host "│  4. Retour au menu précédent                     │"
+        Write-Host "│                                                  │"
+        Write-Host "╰──────────────────────────────────────────────────╯"
+        Write-Host ""
+        
+        $groupMainMenu = Read-Host "► Choisissez une option"
+
+        switch ($groupMainMenu) {
+
+            1 {
+                logEvent "MENU_GROUPES:AJOUT_D'_UTILISATEUR_AU_GROUPE_ADMIN"
+                add_user_admin_group_windows
+            }
+
+            2 {
+                logEvent "MENU_GROUPES:AJOUT_D'UN_UTILISATEUR_À_UN_GROUPE"
+                add_user_group_windows
+            }
+
+            3 {
+                logEvent "MENU_GROUPES:SORTIE_D'UN_UTILISATEUR_D'UN_GROUPE"
+                del_user_group_windows
+            }    
+
+            4 {
+                logEvent "SÉLECTION_RETOUR_AU_MENU_PRÉCÉDENT"
+                Write-Host "Retour au menu précédent"
+                userMainMenu
+            }    
+
+            default {
+                logEvent "OPTION_INVALIDE"
+                Write-Host "► Entrée invalide !"
+            }
+        }
+    }
+}
+
+
+
+#==============================================================
+# 2 - FONCTION AJOUTER UN UTILISATEUR AU GROUPE ADMIN
+#==============================================================
+function add_user_admin_group_windows {
+
+    logEvent "MENU_AJOUT_GROUPE_ADMIN"
+
+    Write-Host ""
+    Write-Host "╭──────────────────────────────────────────────────╮"
+    Write-Host "│      AJOUTER UTILISATEUR AU GROUPE ADMIN         │"
+    Write-Host "├──────────────────────────────────────────────────┤"
+    Write-Host "│                                                  │"
+    Write-Host "│  1. Saisir le nom d'utilisateur                  │"
+    Write-Host "│  2. Retour au menu précédent                     │"
+    Write-Host "│                                                  │"
+    Write-Host "╰──────────────────────────────────────────────────╯"
+    Write-Host ""
+    
+    $addGroupAdmin = Read-Host "► Choisissez une option"
+
+    switch ($addGroupAdmin) {
+        1 {
+            logEvent "MENU_AJOUT_GROUPE_ADMIN:SAISIE_UTILISATEUR:"
+            
+            ##########################  FONCTION A FAIRE ICI ##########################
+        }
+
+        2 {
+            logEvent "MENU_AJOUT_GROUPE_ADMIN:RETOUR_MENU_PRECEDENT"
+            gestion_menu_group_windows
+        }
+
+        default {
+            logEvent "MENU_AJOUT_GROUPE_ADMIN:OPTION_INVALIDE"
+            Write-Host "► Entrée invalide !"
+        }
+    }
+
+
+}
+
+
+
+#==============================================================
+# 3 - FONCTION AJOUTER UN UTILISATEUR A UN GROUPE
+#==============================================================
+
+function add_user_group_windows {
+
+    logEvent "MENU_AJOUT_GROUPE"
+
+    Write-Host ""
+    Write-Host "╭──────────────────────────────────────────────────╮"
+    Write-Host "│          AJOUTER UTILISATEUR A UN GROUPE         │"
+    Write-Host "├──────────────────────────────────────────────────┤"
+    Write-Host "│                                                  │"
+    Write-Host "│  1. Saisir le nom d'utilisateur                  │"
+    Write-Host "│  2. Retour au menu précédent                     │"
+    Write-Host "│                                                  │"
+    Write-Host "╰──────────────────────────────────────────────────╯"
+    Write-Host ""
+
+    $addGroup = Read-Host "► Choisissez une option"
+
+    switch ($addGroup) {
+        1 {
+            logEvent "MENU_AJOUT_GROUPE:SAISIE_UTILISATEUR:"
+            
+            ##########################  FONCTION A FAIRE ICI ##########################
+        }
+
+        2 {
+            logEvent "MENU_AJOUT_GROUPE:RETOUR_MENU_PRECEDENT"
+            gestion_menu_group_windows
+        }
+
+        default {
+            logEvent "MENU_AJOUT_GROUPE:OPTION_INVALIDE"
+            Write-Host "► Entrée invalide !"
+        }
+    }
+
+}
+
+
+
+
+#==============================================================
+# 4 - FONCTION RETIRER UN UTILISATEUR D'UN GROUPE
+#==============================================================
+
+function del_user_group_windows {
+
+    logEvent "MENU_SUPPRESSION_GROUPE"
+
+    Write-Host ""
+    Write-Host "╭──────────────────────────────────────────────────╮"
+    Write-Host "│      SUPPRIMER UN UTILISATEUR D'UN GROUPE        │"
+    Write-Host "├──────────────────────────────────────────────────┤"
+    Write-Host "│                                                  │"
+    Write-Host "│  1. Saisir le nom d'utilisateur                  │"
+    Write-Host "│  2. Retour au menu précédent                     │"
+    Write-Host "│                                                  │"
+    Write-Host "╰──────────────────────────────────────────────────╯"
+    Write-Host ""
+
+    $delGroup = Read-Host "► Choisissez une option"
+
+    switch ($delGroup) {
+        1 {
+            logEvent "MENU_SUPPRESSION_GROUPE:SAISIE_UTILISATEUR:"
+            
+            ##########################  FONCTION A FAIRE ICI ##########################
+        }
+
+        2 {
+            logEvent "MENU_SUPPRESSION_GROUPE:RETOUR_MENU_PRECEDENT"
+            gestion_menu_group_windows
+        }
+
+        default {
+            logEvent "MENU_SUPPRESSION_GROUPE:OPTION_INVALIDE"
+            Write-Host "► Entrée invalide !"
+        }
+    }
+
+}
---
+++ Ressources/scripts_powershell_modules/windows/03_scriptGestionOrdiWindows.ps1
@@ -0,0 +1,182 @@
+﻿# Script Gestion ordinateur Windows en Powershell
+
+
+# Liste des fonctions :
+# 1. Menu Gestion Répertoire
+# 2. Création de répertoire
+# 3. Création de répertoire Linux (SUDO)
+# 4. Suppression de répertoire
+# 5. Redémmarage
+# 6. Prise en main à distance (CLI)
+# 7. Activation du pare-feu
+# 8. Exécution de scripts sur une machine distante
+
+
+#==============================================================
+# 1 - MENU GESTION REPERTOIRE
+#==============================================================
+function gestion_repertoire_menu_windows {
+
+    logEvent "MENU_GESTION_RÉPERTOIRE"
+    
+    while ($true) {
+
+        Write-Host ""
+        Write-Host "╭──────────────────────────────────────────────────╮" 
+        Write-Host "│               GESTION REPERTOIRES                │"
+        Write-Host "├──────────────────────────────────────────────────┤" 
+        Write-Host "│                                                  │"
+        Write-Host "│  1. Créer un répertoire                          │" 
+        Write-Host "│  2. Créer une répertoire (SUDO LINUX)            │" 
+        Write-Host "│  3. Supprimer un répertoire                      │" 
+        Write-Host "│  4. Retour au menu précédent                     │" 
+        Write-Host "│                                                  │"
+        Write-Host "╰──────────────────────────────────────────────────╯"
+        Write-Host ""
+        
+        $repertoireMainMenu = Read-Host "► Choisissez une option "
+
+        switch ($repertoireMainMenu) {
+
+            1 {
+                logEvent "SÉLECTION_CRÉATION_DE_DOSSIER"
+                # A CHANGER
+                creer_dossier_windows
+            }
+
+            2 {
+                logEvent "SÉLECTION_CRÉATION_DE_DOSSIER_SUDO"
+                # A CHANGER
+                creer_dossier_admin_windows
+            }
+
+            3 {
+                logEvent "SÉLECTION_SUPPRESSION_DE_DOSSIER"
+                # A CHANGER
+                supprimer_dossier_windows
+            }    
+
+            4 {
+                logEvent "SÉLECTION_RETOUR_AU_MENU_PRÉCÉDENT"
+                # A CHANGER
+                Write-Host "Retour au menu précédent"
+                computerMainMenu
+            }    
+
+            default {
+                logEvent "OPTION_INVALIDE"
+                Write-Host "► Entrée invalide !"
+
+            }
+        }
+    }
+}
+
+
+
+
+#==============================================================
+# 2 - FONCTION CREATION DE REPERTOIRE
+#==============================================================
+function creer_dossier_windows {
+
+    logEvent "CRÉATION_DE_DOSSIER"
+
+    Write-Host "Entrez le chemin du dossier "
+    $creation_dossier = Read-Host "►"
+
+    ##########################  FONCTION A FAIRE ICI ##########################
+
+}
+
+
+
+#==============================================================
+# 3 - FONCTION CREATION DE REPERTOIRE SUDO LINUX
+#==============================================================
+
+function creer_dossier_admin_windows {
+
+    logEvent "CRÉATION_DE_DOSSIER"
+
+    Write-Host "Entrez le chemin du dossier "
+    $creation_dossier_sudo = Read-Host "►"
+
+    ##########################  FONCTION A FAIRE ICI ##########################
+
+}
+
+
+
+#==============================================================
+# 4 - FONCTION SUPPRESSION DE REPERTOIRE
+#==============================================================
+
+function supprimer_dossier_windows {
+
+    logEvent "CRÉATION_DE_DOSSIER"
+    Write-Host "Entrez le chemin du dossier "
+
+    $suppression_dossier = Read-Host "►"
+
+    ##########################  FONCTION A FAIRE ICI ##########################
+
+}
+
+
+
+#==============================================================
+# 5 - FONCTION REDEMARRAGE
+#==============================================================
+function redemarrage_windows {
+
+    logEvent "DEMANDE_VOULEZ_VOUS_REDEMARRER_L'ORDINATEUR"
+    $restartComputer = Read-Host "► Voulez-vous redémarrer l'ordinateur distant ? (o/n) "
+
+    ##########################  FONCTION A FAIRE ICI ##########################
+
+}
+
+
+
+#==============================================================
+# 6 - FONCTION PRISE EN MAIN A DISTANCE (CLI)
+#==============================================================
+function prise_main_windows {
+
+    logEvent "DEMANDE_PRISE_DE_MAIN_DISTANTE_SSH"
+    # FONCTION SSH A AJOUTER
+
+}
+
+
+
+#==============================================================
+# 7 - FONCTION ACTIVATION PARE-FEU
+#==============================================================
+function activer_parefeu_windows {
+
+    logEvent "DEMANDE_ACTIVATION_UFW"
+    Write-Host "► Activation du pare-feu"
+
+    $activationParefeu = Read-Host "► Voulez-vous activer le pare-feu UFW ? (o/n) "
+
+    ##########################  FONCTION A FAIRE ICI ##########################
+
+}
+
+
+
+#==============================================================
+# 8 - FONCTION EXECUTION DE SCRIPT LOCAL
+#==============================================================
+function exec_script_windows {
+
+    logEvent "DEMANDE_CHEMIN_SCRIPT"
+    Write-Host "► Entrez le chemin du script local à exécuter "
+
+    $scriptLocal = Read-Host "► "
+
+    ##########################  FONCTION A FAIRE ICI ##########################
+
+}

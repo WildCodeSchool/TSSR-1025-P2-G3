d8631ae55a2d2d7ab9e6e70fd77b6b4404a7e37f
christianwildcodeschool-dotcom <christian.a.wildcodeschool@gmail.com>
2025-12-16 16:42:41 +0100

    Update 05_scriptUsersInfosLinux.sh

--- Ressources/scripts_bash_modules/linux/05_scriptUsersInfosLinux.sh
+++ Ressources/scripts_bash_modules/linux/05_scriptUsersInfosLinux.sh
@@ -4,9 +4,9 @@
 # Auteur : Pierre-Jean
 
 # Sommaire :
-# 01. Date dernière connexion
-# 02. Date dernière modification mot de passe
-# 03. Liste des sessions ouvertes
+# 01 - Date dernière connexion
+# 02 - Date dernière modification mot de passe
+# 03 - Liste des sessions ouvertes
 
 
 #==============================================================

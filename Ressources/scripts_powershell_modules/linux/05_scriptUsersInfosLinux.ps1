7abfbfe7d7ebfff44a0bb036ab79cf1602756853
christianwildcodeschool-dotcom <christian.a.wildcodeschool@gmail.com>
2025-12-16 16:48:27 +0100

    Update 05_scriptUsersInfosLinux.ps1

--- Ressources/scripts_powershell_modules/linux/05_scriptUsersInfosLinux.ps1
+++ Ressources/scripts_powershell_modules/linux/05_scriptUsersInfosLinux.ps1
@@ -1,6 +1,7 @@
 ﻿# Script Information Utilisateurs Linux en Powershell
 # Auteur : Pierre-Jean
 
+
 # Sommaire :
 # 01. Date dernière connexion
 # 02. Date dernière modification mot de passe
@@ -270,4 +271,5 @@ function list_opensessions_linux {
     }
 }
 #endregion
+
 
